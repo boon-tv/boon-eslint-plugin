bdb8a8be7a2bf29e9f470afd7640ddbc21ff0ff2
Morten Jelle <1551620+appelsinvandi@users.noreply.github.com>
2022-09-08 15:15:03 +0200

    feat: publish to reg on release

--- dist/configs/recommended.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const util_1 = require("../util");
-exports.default = (0, util_1.createPreset)({
-    'strict-short-circuit-expressions': ['error']
-});
